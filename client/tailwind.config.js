d1f389817ae129a9d7d6fc44040946ba6c2502da
logan-larson <loganlarson20@gmail.com>
2022-11-15 14:25:53 -0600

    Micro redesign

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,16 +5,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      dropShadow: {
-        'violating': '0 0 25px red',
-        'greeter': '0 0 25px rgb(34 197 94)',
-        'ask': '0 0 25px rgb(96 165 250)',
-        'remark': '0 0 25px rgb(244 114 182)',
-        'instruction': '0 0 25px rgb(252 211 77)',
-        'handoff': '0 0 25px rgb(100 116 139)',
-        'answer': '0 0 25px rgb(147 51 234)',
-        'wait': '0 0 25px rgb(251 146 60)',
-        'farewell': '0 0 25px rgb(248 113 113)'
+      boxShadow: {
+        'violating': '0 0 5px 5px red',
+        'greeter': '0 0 5px 5px rgb(34 197 94)',
+        'ask': '0 0 5px 5px rgb(96 165 250)',
+        'remark': '0 0 5px 5px rgb(244 114 182)',
+        'instruction': '0 0 5px 5px rgb(252 211 77)',
+        'handoff': '0 0 5px 5px rgb(100 116 139)',
+        'answer': '0 0 5px 5px rgb(147 51 234)',
+        'wait': '0 0 5px 5px rgb(251 146 60)',
+        'farewell': '0 0 5px 5px rgb(248 113 113)'
       }
     },
   },
