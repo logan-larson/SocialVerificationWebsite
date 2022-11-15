d5a9921d4a2720d1c86e149816d54654fa760e9b
logan-larson <loganlarson20@gmail.com>
2022-11-15 14:11:25 -0600

    Param refactor

--- server/src/models/parameter.ts
+++ server/src/models/parameter.ts
@@ -1,19 +1,16 @@
 export class Parameter {
 
-    id: number;
     title: string; // the name that will display for this parameter
     variableName: string; // the variable name associated with this parameter as defined in back end
     description: string;
     type: string // one of these: "str", "bool", "array","int" - (these are sent to back end as parameter type)
 
     constructor(
-        id: number = -1, 
         title: string = '',
         variableName: string = '', 
         description: string = '', 
         type: string = ''
     ) {
-        this.id = id;
         this.title = title;
         this.variableName = variableName;
         this.description = description;
--- server/src/models/trackedMicroTypes.ts
+++ server/src/models/trackedMicroTypes.ts
@@ -5,49 +5,52 @@ import { Parameter } from "./parameter";
 let microTypes: MicroType[] = [];
 
 //greeter
-let parameter0 = new Parameter(0, "Wait for Response", "Wait_for_response", "Set whether the robot waits for the human to greet back", "bool");
-//let parameter1 = new Parameter(1, "Greet with Speech", "Greet_with_speech", "Set whether the robot greets the human with speech", "bool");
-let parameter1 = new Parameter(1, "Greet with Handshake", "Greet_with_handshake", "Set whether the robot extends its arm for a handshake", "bool");
-let microGreeter = new MicroType("Greeter", [parameter0, parameter1]);
+let microGreeter = new MicroType("Greeter", [
+  new Parameter("Wait for Response", "Wait_for_response", "Set whether the robot waits for the human to greet back", "bool"),
+  new Parameter("Greet with Handshake", "Greet_with_handshake", "Set whether the robot extends its arm for a handshake", "bool")
+]);
 microTypes.push(microGreeter);
 
 //ask
-let parameter3 = new Parameter(3, "Question", "question", "The Specific question the robot will ask", "str");
-let parameter4 = new Parameter(4, "Ready Reponse", "responseReady", "The expeceted response of a human in the Ready state", "str");
-let parameter13 = new Parameter(13, "Not Ready Response", "responseNotReady", "The expected response of a human in the Not Ready state", "str");
-//let parameter4 = new Parameter(4, "Responses the robot can recognize", "answers robot can recognize", "input the answers the robot can recognize the user saying, and then set the state that should be executed following the response", "array");
-let microAsk = new MicroType("Ask", [parameter3, parameter4, parameter13]);
+let microAsk = new MicroType("Ask", [
+  new Parameter("Question", "question", "The Specific question the robot will ask", "str"),
+  new Parameter("Ready Reponse", "responseReady", "The expeceted response of a human in the Ready state", "str"),
+  new Parameter("Not Ready Response", "responseNotReady", "The expected response of a human in the Not Ready state", "str")
+]);
 microTypes.push(microAsk);
 
 //remark
-let parameter5 = new Parameter(5, "Content", "content", "What the robot will say to the user", "str");
-let parameter6 = new Parameter(6, "Use Gesture", "use_gesture", "Should the robot use gestures (this is different from handoff)", "bool");
-let parameter7 = new Parameter(7, "Allow the human to respond", "Allow_human_to_respond", "Whether the robot gives the human any time to respond after the robot's remark before moving on", "bool");
-let microRemark = new MicroType("Remark", [parameter5, parameter6, parameter7]);
+let microRemark = new MicroType("Remark", [
+  new Parameter("Content", "content", "What the robot will say to the user", "str"),
+  new Parameter("Use Gesture", "use_gesture", "Should the robot use gestures (this is different from handoff)", "bool"),
+  new Parameter("Allow the human to respond", "Allow_human_to_respond", "Whether the robot gives the human any time to respond after the robot's remark before moving on", "bool")
+]);
 microTypes.push(microRemark);
 
 //instruction
-let parameter8 = new Parameter(8, "Instruction", "Instruction", "The instruction that the robot will provide the human", "str");
-let microInstruct = new MicroType("Instruction", [parameter8]);
+let microInstruct = new MicroType("Instruction", [
+  new Parameter("Instruction", "Instruction", "The instruction that the robot will provide the human", "str")
+]);
 microTypes.push(microInstruct);
 
 //handoff
-// ** This uses new preferred way of adding parameters to a micro **
 let microHandoff = new MicroType("Handoff", [
-  new Parameter(2, "Robot Speaks", "robot_speaks", "Robot speaks during handoff", "bool")
+  new Parameter("Robot Speaks", "robot_speaks", "Robot speaks during handoff", "bool")
 ]);
 microTypes.push(microHandoff);
 
 //answer
-let parameter9 = new Parameter(9, "Introduction", "Introduction", "Robot begins microinteraction by saying I can answer your question", "bool");
-let microAnswer = new MicroType("Answer", [parameter9]);
+let microAnswer = new MicroType("Answer", [
+  new Parameter("Introduction", "Introduction", "Robot begins microinteraction by saying I can answer your question", "bool")
+]);
 microTypes.push(microAnswer);
 
 //wait
-let parameter10 = new Parameter(10, "Wait Time (seconds)", "wait time (seconds)", "Number of seconds for the robot to wait", "int");
-let parameter11 = new Parameter(11, "Allow Speech", "allow_speech", "Allows a human to say something to the robot to override its wait time", "bool");
-let parameter12 = new Parameter(12, "Look At People", "look_at_people", "Enable face tracking, which allows the robot to met the gaze of anyone in its vicinity", "bool");
-let microWait = new MicroType("Wait", [parameter10, parameter11, parameter12]);
+let microWait = new MicroType("Wait", [
+  new Parameter("Wait Time (seconds)", "wait time (seconds)", "Number of seconds for the robot to wait", "int"),
+  new Parameter("Allow Speech", "allow_speech", "Allows a human to say something to the robot to override its wait time", "bool"),
+  new Parameter("Look At People", "look_at_people", "Enable face tracking, which allows the robot to met the gaze of anyone in its vicinity", "bool")
+]);
 microTypes.push(microWait);
 
 //farwell
