a36b47ff537df7143d8a4a1e04b38bda612abda2
logan-larson <loganlarson20@gmail.com>
2023-02-06 07:37:58 -0600

    Fixed app sizing and responsivity

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,7 +4,7 @@ import {CanvasManagerService} from './services/canvas-manager.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 export class AppComponent {
   title = 'client';
--- client/src/app/components/designer/designer.component.ts
+++ client/src/app/components/designer/designer.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, } from '@angular/core';
 @Component({
   selector: 'app-designer',
   templateUrl: './designer.component.html',
-  styleUrls: []
 })
 export class DesignerComponent implements OnInit {
 
